5f3657c227ae09b76621192b751e0d1e07fdc5b8
HankB-o-t <t37.slime.1.1@gmail.com>
2024-08-20 20:47:41 -0300

    aaa

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 use rocket_dyn_templates::{Template, context};
 mod req;
 
+//  What this ugly ahh code does.  
+//  This code is just basically the server
+//  The important shit is on the req.rs file
+//  inefficient ahh code
+
 #[get("/")]
 async fn index() -> Template {
     let dolar_c = req::precio_compra().await.unwrap();
